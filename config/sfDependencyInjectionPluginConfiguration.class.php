5de7a90f2e9f8add1a9fd0f321839f8502ff0165
Issei.M <issei.m7@gmail.com>
2013-10-13 17:11:45 +0900

    first commit

---
+++ config/sfDependencyInjectionPluginConfiguration.class.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+ * This file is part of the sfDependencyInjectionPlugin package.
+ * (c) Issei Murasawa <issei.m7@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once sfConfig::get('sf_root_dir') . '/psr/vendor/autoload.php';
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\ConfigCache;
+
+/**
+ * The configuration of sfDependencyInjectionPlugin.
+ *
+ * Integration with Symfony's DependencyInjection component like the HttpKernel component.
+ *
+ * @package    sfPsrSupportPlugin
+ * @subpackage config
+ * @author     Issei Murasawa <issei.m7@gmail.com>
+ * @link       https://github.com/symfony/DependencyInjection
+ * @link       https://github.com/symfony/HttpKernel
+ */
+class sfDependencyInjectionPluginConfiguration extends sfPluginConfiguration
+{
+    /**
+     * @see sfPluginConfiguration
+     */
+    public function initialize()
+    {
+        $this->dispatcher->connect('context.load_factories', function(sfEvent $event) {
+            if (sfConfig::get('sf_debug') && sfConfig::get('sf_logging_enabled')) {
+                $timer = sfTimerManager::getTimer('Initialize ServiceContainer');
+            }
+
+            $context = $event->getSubject();
+            $context->set('container', $this->initializeContainer());
+
+            if (sfConfig::get('sf_debug') && sfConfig::get('sf_logging_enabled')) {
+                $timer->addTime();
+            }
+        });
+    }
+
+    /**
+     * Initializes the service container.
+     *
+     * The cached version of the service container is used when fresh, otherwise the
+     * container is built.
+     *
+     * @return ContainerBuilder
+     */
+    protected function initializeContainer()
+    {
+        $class = $this->getContainerClass();
+        $cache = new ConfigCache(sfConfig::get('sf_app_cache_dir') . '/' . $class . '.php', sfConfig::get('sf_debug'));
+
+        if (!$cache->isFresh()) {
+            $container = $this->buildContainer();
+            $container->compile();
+
+            $this->dumpContainer($cache, $container, $class, $this->getContainerBaseClass());
+
+            sfContext::getInstance()->getLogger()->info('Initialized the ServiceContainer');
+        }
+
+        require_once $cache;
+
+        $container = new $class();
+
+        return $container;
+    }
+
+    /**
+     * Gets the container class.
+     *
+     * @return string The container class
+     */
+    protected function getContainerClass()
+    {
+        return (sfConfig::get('sf_debug') ? 'Debug' : '') . 'ProjectContainer';
+    }
+
+    /**
+     * Gets the container's base class.
+     *
+     * All names except Container must be fully qualified.
+     *
+     * @return string
+     */
+    protected function getContainerBaseClass()
+    {
+        return 'Container';
+    }
+
+    /**
+     * Builds the service container.
+     *
+     * @return ContainerBuilder The compiled service container
+     *
+     */
+    protected function buildContainer()
+    {
+        $container = new ContainerBuilder();
+
+        $loader = new YamlFileLoader($container, new FileLocator(sfConfig::get('sf_config_dir')));
+        $loader->load('services.yml');
+
+        return $container;
+    }
+
+    /**
+     * Dumps the service container to PHP code in the cache.
+     *
+     * @param ConfigCache      $cache     The config cache
+     * @param ContainerBuilder $container The service container
+     * @param string           $class     The name of the class to generate
+     * @param string           $baseClass The name of the container's base class
+     */
+    protected function dumpContainer(ConfigCache $cache, ContainerBuilder $container, $class, $baseClass)
+    {
+        // cache the container
+        $dumper = new PhpDumper($container);
+
+        $content = $dumper->dump(array('class' => $class, 'base_class' => $baseClass));
+        if (!sfConfig::get('sf_debug')) {
+            $content = self::stripComments($content);
+        }
+
+        $cache->write($content, $container->getResources());
+    }
+
+    /**
+     * Removes comments from a PHP source string.
+     *
+     * We don't use the PHP php_strip_whitespace() function
+     * as we want the content to be readable and well-formatted.
+     *
+     * @param string $source A PHP string
+     *
+     * @return string The PHP string with the comments removed
+     */
+    public static function stripComments($source)
+    {
+        if (!function_exists('token_get_all')) {
+            return $source;
+        }
+
+        $rawChunk = '';
+        $output = '';
+        $tokens = token_get_all($source);
+        for (reset($tokens); false !== $token = current($tokens); next($tokens)) {
+            if (is_string($token)) {
+                $rawChunk .= $token;
+            } elseif (T_START_HEREDOC === $token[0]) {
+                $output .= preg_replace(array('/\s+$/Sm', '/\n+/S'), "\n", $rawChunk).$token[1];
+                do {
+                    $token = next($tokens);
+                    $output .= $token[1];
+                } while ($token[0] !== T_END_HEREDOC);
+                $rawChunk = '';
+            } elseif (!in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
+                $rawChunk .= $token[1];
+            }
+        }
+
+        // replace multiple new lines with a single newline
+        $output .= preg_replace(array('/\s+$/Sm', '/\n+/S'), "\n", $rawChunk);
+
+        return $output;
+    }
+}
