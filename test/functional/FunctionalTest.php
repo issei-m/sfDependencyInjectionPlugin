1136095c61f90640a367241be8677e618a403aa7
Issei.M <issei.m7@gmail.com>
2015-05-10 02:29:39 +0900

    added test cases

--- test/functional/FunctionalTest.php
+++ test/functional/FunctionalTest.php
@@ -11,12 +11,27 @@ class FunctionalTest extends PHPUnit_Framework_TestCase
         /** @var sfContainer $container */
         $container = sfContext::getInstance()->getServiceContainer();
 
+        $this->assertEquals('app1DebugContainer', get_class($container));
         $this->assertTrue($container->getParameter('extended'));
         $this->assertEquals('App1', $container->getParameter('name'));
         $this->assertInstanceOf('TestClass', $container->get('test'));
         $this->assertEquals('A', $container->get('test')->a);
         $this->assertEquals('B', $container->get('test')->b);
         $this->assertEquals('C', $container->get('test')->c);
+    }
+
+    public function testNoDebug()
+    {
+        sfContext::createInstance(sfProjectConfiguration::getApplicationConfiguration('app2', 'test', false));
+
+        /** @var sfContainer $container */
+        $container = sfContext::getInstance()->getServiceContainer();
+
+        $this->assertEquals('app2Container', get_class($container));
+        $this->assertEquals('App2', $container->getParameter('name'));
+        $this->assertEquals('X', $container->get('test')->a);
+        $this->assertEquals('Y', $container->get('test')->b);
+        $this->assertEquals('Z', $container->get('test')->c);
     }
 
     public function tearDown()
