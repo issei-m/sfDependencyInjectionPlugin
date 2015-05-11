2447603497af0ba9521a5265a428cfc95f87c12d
Issei.M <issei.m7@gmail.com>
2015-05-11 12:49:34 +0900

    fixed broken test

--- test/unit/container/sfCompatibleWithSymfony15ContainerTest.php
+++ test/unit/container/sfCompatibleWithSymfony15ContainerTest.php
@@ -6,17 +6,17 @@ class sfCompatibleWithSymfony15ContainerTest extends \PHPUnit_Framework_TestCase
 {
     public function testGeneral()
     {
-        $container = new sfCompatibleWithSymfony15Container(new ParameterBag(['one' => 1]));
+        $container = new sfCompatibleWithSymfony15Container(new ParameterBag(array('one' => 1)));
         $this->assertFalse($container->hasService('foo_service'));
-        $this->assertSame(['one' => 1], $container->getParameters());
+        $this->assertSame(array('one' => 1), $container->getParameters());
 
         $container->setService('foo_service', $fooService = new \stdClass());
-        $container->addParameters(['two' => 2, 'three' => 3]);
+        $container->addParameters(array('two' => 2, 'three' => 3));
         $this->assertTrue($container->hasService('foo_service'));
         $this->assertSame($fooService, $container->getService('foo_service'));
-        $this->assertSame(['one' => 1, 'two' => 2, 'three' => 3], $container->getParameters());
+        $this->assertSame(array('one' => 1, 'two' => 2, 'three' => 3), $container->getParameters());
 
-        $container->setParameters(['ten' => 10]);
-        $this->assertSame(['ten' => 10], $container->getParameters());
+        $container->setParameters(array('ten' => 10));
+        $this->assertSame(array('ten' => 10), $container->getParameters());
     }
 }
